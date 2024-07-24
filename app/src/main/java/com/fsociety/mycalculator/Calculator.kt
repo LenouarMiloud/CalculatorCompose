7c20e47d5fd4431151b2057b81ba72785a421005
LenouarMiloud <lenouarmiloud@gmail.com>
2024-07-24 12:39:34 -0700

    design the buttons of the Calculator

--- app/src/main/java/com/fsociety/mycalculator/Calculator.kt
+++ app/src/main/java/com/fsociety/mycalculator/Calculator.kt
@@ -6,13 +6,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -54,10 +60,32 @@ fun Calculator(modifier : Modifier = Modifier){
             
             LazyVerticalGrid(columns = GridCells.Fixed(4),) {
                 items(buttonList){
-                    Text(text = it)
+                    ButtonCalculator(btn = it)
                 }
             }
             
         }
     }
+}
+
+@Composable
+fun ButtonCalculator(btn: String){
+    Box(modifier = Modifier.padding(8.dp)){
+        FloatingActionButton(onClick = {  },
+            modifier = Modifier.size(100.dp),
+            shape = CircleShape,
+            contentColor = Color.White,
+            containerColor = Color.Red) {
+            Text(text = btn)
+        }
+    }
+}
+@Composable
+fun getColor (btn: String): Color{
+    if(btn == "C" || btn== "AC")
+        return Color(0xFFF44336)
+    if(btn == "(" || btn == ")")
+        return Color.Gray
+    if(btn == "/" || btn == "=" || btn == "+" || btn == "-")
+        return Color(0xFF)
 }
