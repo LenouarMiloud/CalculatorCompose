10891f7ef0bde6a5f1afd7e00b03cf59182a79fc
LenouarMiloud <lenouarmiloud@gmail.com>
2024-07-23 17:21:38 -0700

    add the calculator tools

--- app/src/main/java/com/fsociety/mycalculator/Calculator.kt
+++ app/src/main/java/com/fsociety/mycalculator/Calculator.kt
@@ -3,7 +3,12 @@ package com.fsociety.mycalculator
 import android.graphics.Paint.Style
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,7 +16,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+val buttonList = listOf(
+    "C","(",")","/",
+    "7","8","9","*",
+    "4","5","6","+",
+    "1","2","3","-",
+    "AC","0",".","=",
+)
 
 @Composable
 fun Calculator(modifier : Modifier = Modifier){
@@ -28,7 +42,22 @@ fun Calculator(modifier : Modifier = Modifier){
                 maxLines = 5,
                 overflow = TextOverflow.Ellipsis
             )
-
+            Text(
+                text = "4134",
+                style = TextStyle(
+                    fontSize = 60.sp,
+                    textAlign = TextAlign.End
+                ),
+                maxLines = 2,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            
+            LazyVerticalGrid(columns = GridCells.Fixed(4),) {
+                items(buttonList){
+                    Text(text = it)
+                }
+            }
+            
         }
     }
 }
